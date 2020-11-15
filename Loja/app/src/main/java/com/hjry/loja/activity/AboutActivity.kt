3043d32858e27a16b47313d88eabbeb14d73a8d2
higor2100 <42756331+higor2100@users.noreply.github.com>
2020-11-15 15:25:56 -0300

    Revert

--- Loja/app/src/main/java/com/hjry/loja/activity/AboutActivity.kt
+++ Loja/app/src/main/java/com/hjry/loja/activity/AboutActivity.kt
@@ -1,7 +1,5 @@
 package com.hjry.loja.activity
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.hjry.loja.R
@@ -12,25 +10,9 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
-        val actionBar =supportActionBar
-        actionBar!!.title = resources.getString(R.string.about)
-        actionBar.setDisplayHomeAsUpEnabled(true)
-
-        txtEmail.setOnClickListener {
-            val emailIntent = Intent(
-                Intent.ACTION_SENDTO, Uri.fromParts(
-                    "mailto", "hjrysenac@gmail.com", null
-                )
-            )
-            startActivity(Intent.createChooser(emailIntent, resources.getString(R.string.send_email)))
+        ivBack.setOnClickListener {
+            onBackPressed()
         }
-        txtSite.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW,Uri.parse("https://www.sp.senac.br/"))
-            startActivity(intent)
-        }
-
-
-
     }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
