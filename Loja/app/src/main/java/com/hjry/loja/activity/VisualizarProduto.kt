1f00e7ce2dc9ea6378f14f276faafbe6ac82a9c6
higor2100 <42756331+higor2100@users.noreply.github.com>
2020-12-05 15:19:50 -0300

    NavigationView
    Add NV para categorias

--- Loja/app/src/main/java/com/hjry/loja/activity/VisualizarProduto.kt
+++ Loja/app/src/main/java/com/hjry/loja/activity/VisualizarProduto.kt
@@ -6,20 +6,25 @@ import com.hjry.loja.R
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_visualizar_produto.*
 import kotlinx.android.synthetic.main.cartao_lista_produto.view.*
+import java.text.NumberFormat
 
 class VisualizarProduto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visualizar_produto)
+
+        val formatter = NumberFormat.getCurrencyInstance()
+
         var id = intent.getStringExtra("id")
         val title = intent.getStringExtra("title")
         val desc = intent.getStringExtra("desc")
         val qtd = intent.getStringExtra("estoque")
         val preco = intent.getStringExtra("preco")
 
+
         txtNomeVisualizar.text = title
         textDescProduto.text = desc
-        txtPrecoVisualizar.text = preco
+        txtPrecoVisualizar.text = formatter.format(preco!!.toFloat())
         txtQtd.text = qtd
         Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/projetopdm-3d833.appspot.com/o/photos%2F" + id + ".jpg?alt=media").into(
             imageProduto
