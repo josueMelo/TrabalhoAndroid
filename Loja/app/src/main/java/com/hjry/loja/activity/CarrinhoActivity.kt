d0096e1ee612a6486c4d43bc5ef2c212594104d2
josue <josue.melomatos@gmail.com>
2020-12-06 15:16:47 -0300

    Correção de fechamento da aplicação

--- Loja/app/src/main/java/com/hjry/loja/activity/CarrinhoActivity.kt
+++ Loja/app/src/main/java/com/hjry/loja/activity/CarrinhoActivity.kt
@@ -1,12 +1,133 @@
 package com.hjry.loja.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.SearchView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.hjry.loja.Nav
 import com.hjry.loja.R
+import com.hjry.loja.adapter.ProdutosRecyclerViewAdapter
+import com.hjry.loja.model.Produto
+import kotlinx.android.synthetic.main.activity_lista_produto.*
 
 class CarrinhoActivity : AppCompatActivity() {
+
+    var adapter: ProdutosRecyclerViewAdapter? = null
+    var database: DatabaseReference? = null
+    var toggle: ActionBarDrawerToggle? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carrinho)
     }
 }
+//
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        toggle = ActionBarDrawerToggle(
+//            this, DL,
+//            R.string.Open, R.string.Close
+//        )
+//        toggle?.let {
+//            DL.addDrawerListener(it)
+//            it.syncState()
+//        }
+//        NV.setNavigationItemSelectedListener {
+//            DL.closeDrawers()
+//            if (it.itemId == R.id.Cell) {
+//                val frag = Nav.newInstance("", "")
+//                supportFragmentManager.beginTransaction().replace(R.id.fragContainer, frag).commit()
+//
+//                true
+//            }
+//            false
+//        }
+//
+//
+//
+//        configureDatabase("")
+
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//val inflater: MenuInflater = menuInflater
+//inflater.inflate(R.menu.menu_lista_produto, menu)
+//val menuitem = menu?.findItem(R.id.btnCarrinho) as MenuItem
+//val menuIte = menu?.findItem(R.id.about) as MenuItem
+
+//val btnBusca = menuitem.actionView as SearchView
+
+//        btnBusca.queryHint = getString(R.string.NameProduct)
+//
+//        btnBusca.setOnQueryTextListener(object
+//            : SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                btnBusca.clearFocus()
+//                configureDatabase(query)
+//                return true
+//            }
+
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                configureDatabase(newText)
+//                return true
+//            }
+
+//        })
+
+//        menuIte.setOnMenuItemClickListener {
+//            val i = Intent(this,AboutActivity::class.java)
+//            startActivity(i)
+//            true
+//        }
+
+
+//        return true
+//    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//
+//
+//        toggle?.let {
+//            return it.onOptionsItemSelected(item)
+//        }
+//        return false
+//    }
+//
+//
+//    fun configureDatabase(busca: String?) {
+//
+//        database = FirebaseDatabase.getInstance().reference.child("produtos")
+//        Log.d("debug", "" + database)
+//
+//
+//        database?.let {
+//            val options = FirebaseRecyclerOptions.Builder<Produto>()
+//                .setQuery(
+//                    it.orderByChild("nome").startAt(busca).endAt(busca + "\uf8ff"),
+//                    Produto::class.java
+//                )
+//                .build()
+//            adapter = ProdutosRecyclerViewAdapter(options)
+//            recyclerView.layoutManager = LinearLayoutManager(this)
+//            recyclerView.adapter = adapter
+//            adapter?.startListening()
+//        }
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        adapter?.stopListening()
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        adapter?.startListening()
+//    }
+//}
